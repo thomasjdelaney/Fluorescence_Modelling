3e3431ac54da1d4a69b9f6db2072fa9275133497
Thomas Delaney <thomas.delaney@posteo.net>
2019-09-14 22:14:16 +0100

    Updating to Julia1.2.0

--- jl/build_command_from_opt.jl
+++ jl/build_command_from_opt.jl
@@ -29,12 +29,12 @@ function main()
   params = parseParams()
   if params["debug"]; info(" Entering debug mode."); return nothing; end
   opt_frame = CSV.read(params["opt_file"])
-  opt_row = opt_frame[opt_frame[:rms] .== minimum(opt_frame[:rms]),:]
+  opt_row = opt_frame[opt_frame[!, :rms] .== minimum(opt_frame[!, :rms]),:]
   param_string = ""
   for p in params["opt_params"]
-    param_string = param_string * " --" * p * " " * string(opt_row[Symbol(p)][1])
+    param_string = param_string * " --" * p * " " * string(opt_row[!, Symbol(p)][1])
   end
-  print(param_string)
+  println(param_string)
 end
 
 main()
--- jl/fluorescence_training.jl
+++ jl/fluorescence_training.jl
@@ -16,15 +16,16 @@ using DataFrames
 using JuliaUtils: rms, getPowerSpectrum, smoothen
 using BlackBoxOptim
 using StatsBase
+using Dates
 
 function getDataModelRMS(configurable_parameters)
   for i = 1:length(configurable_parameters)
     parameter_to_configure = string(params["opt_params"][i])
     params[parameter_to_configure] = configurable_parameters[i]
   end
-  spike_train = convert(Array{Int64,1}, CSV.read(params["spike_file"])[params["colname"]])
+  spike_train = convert(Array{Int64,1}, CSV.read(params["spike_file"])[!, params["colname"]])
   fluorescence, spiking_sim, spiking_sim_time = calciumFluorescenceModel(spike_train, calcium_rate=params["calcium_rate"], indicator=params["indicator"], endogeneous=params["endogeneous"], immobile=params["immobile"], b_i=params["b_i"], f_i=params["f_i"], b_e=params["b_e"], f_e=params["f_e"], b_im=params["b_im"], f_im=params["f_im"], excitation=params["excitation"], release=params["release"], peak=params["peak"], frequency=params["frequency"], capture_rate=params["capture_rate"])
-  fl_trace = CSV.read(params["calcium_file"])[params["colname"]]
+  fl_trace = CSV.read(params["calcium_file"])[!, params["colname"]]
   zscore_fluorescence = zscore(fluorescence)
   zscore_fl = zscore(fl_trace)
   model_freq_array, model_power = getPowerSpectrum(zscore_fluorescence, params["frequency"])
@@ -37,9 +38,9 @@ function getDataModelRMS(configurable_parameters)
 end
 
 function main_opt()
-  info(" Starting training function...", prefix=string(now(), " INFO: "))
+  @info(string(now(), " INFO: ",  " Starting training function..."))
   global params = parseParams()
-  if params["debug"]; info(" Entering debug mode.", prefix=string(now(), " INFO: ")); return nothing; end
+  if params["debug"]; @info(string(now(), " INFO: ", " Entering debug mode.")); return nothing; end
   opt_methods = [:adaptive_de_rand_1_bin_radiuslimited, :xnes, :de_rand_1_bin_radiuslimited, :adaptive_de_rand_1_bin, :generating_set_search, :probabilistic_descent, :de_rand_1_bin, :random_search]
   num_params = length(params["opt_params"])
   num_methods = length(opt_methods)
@@ -48,8 +49,8 @@ function main_opt()
   times = zeros(Float64, num_methods)
   for i = 1:num_methods
     opt_method = opt_methods[i]
-    info(" using $opt_method...", prefix=string(now(), " INFO: "))
-    tic()
+    @info(string(now(), " INFO: ", "using $opt_method..."))
+    start_time = time()
     try 
       res = bboptimize(getDataModelRMS; SearchRange = [(0.0, 1000.0), (0.0, 30.0), (0.0, 30.0), (0.000001, 0.999)], NumDimensions = 4, Method = opt_method, MaxTime = 300.0, TraceMode = :verbose)
       args[i,:] = best_candidate(res)
@@ -58,7 +59,7 @@ function main_opt()
       args[i,:] = zeros(num_params)
       vals[i] = NaN
     end
-    time_taken = toc()
+    time_taken = time() - start_time
     times[i] = time_taken
   end
   opt_frame = DataFrame()
@@ -70,8 +71,8 @@ function main_opt()
   opt_frame[:method] = opt_methods
   filename = ENV["SPACE"] * "/optimisation_csvs/" * replace(params["title"], " "=>"_")
   writetable(filename, opt_frame, header=true)
-  info(" optimisation results written to $filename...", prefix=string(now(), " INFO: "))
-  info(" Done.", prefix=string(now(), " INFO: "))
+  @info(string(now(), " INFO: ", " optimisation results written to $filename..."))
+  @info(string(now(), " INFO: ", " Done."))
 end
 
 main_opt()
